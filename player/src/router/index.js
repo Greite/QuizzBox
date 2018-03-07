11065ec884f4669501d7b753d87f1ca6a3667b7e
leobob70 <leo.bernier70@hotmail.fr>
2018-03-07 16:15:52 +0100

    ok

--- player/src/router/index.js
+++ player/src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Question from '@/components/Question'
 import Accueil from '@/components/Accueil'
+import VueSocketio from 'vue-socket.io';
 
+Vue.use(VueSocketio, 'http://localhost:8080');
 Vue.use(Router)
 
 export default new Router({
