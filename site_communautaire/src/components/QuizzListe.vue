9b016e669b4bdc69e8379e74aed0162d299e425a
Maxime <maximesd52@gmail.com>
2018-03-07 19:03:28 +0100

    css

--- site_communautaire/src/components/QuizzListe.vue
+++ site_communautaire/src/components/QuizzListe.vue
@@ -1,11 +1,24 @@
 <template>
 	<div class="row">
 		<ul class="collection with-header">
-        <li class="collection-header"><h4>Thèmes</h4></li>
-        <li class="collection-item" v-for="theme in themes">{{theme.nom}}
-          <div v-for="quizz in theme.quizz">
-            {{quizz.nom}}
+        <li class="collection-header"><h3>Thèmes</h3></li>
+        <li class="collection-item" v-for="theme in themes">
+          <h4>{{theme.nom}}</h4>
+          <p>{{theme.description}}</p>
+
+      <div class="row" v-for="quizz in theme.quizz">
+        <div class="col s12 m3">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">{{quizz.nom}}</span>
+            </div>
+            <div class="card-action">
+              <a href="#">Télécharger</a>
+            </div>
           </div>
+        </div>
+      </div>
+          
         </li>
       </ul>
 	</div>
