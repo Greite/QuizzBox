255fa8773d52a04b73100e97c0d9cbd58fbdcd3d
Gauthier <core.editor>
2018-03-14 11:41:50 +0100

    Couleur reponses

--- player/src/components/Question.vue
+++ player/src/components/Question.vue
@@ -7,7 +7,7 @@
 					<p>{{questions.quizz[i].texte}}</p>
 				</div>
 				<div class="card-action">
-					<div>
+					<div v-if="preloader">
 						<div class="row">
 							<button class="waves-effect waves-light btn-large col s5" @click="reponse(questions.quizz[i].reponses[0].etat)">{{questions.quizz[i].reponses[0].texte}}</button>
 							<a class="waves-effect waves-light btn-large col s5 offset-s2" @click="reponse(questions.quizz[i].reponses[1].etat)">{{questions.quizz[i].reponses[1].texte}}</a>
@@ -15,6 +15,16 @@
 						<div class="row">
 							<a class="waves-effect waves-light btn-large col s5" @click="reponse(questions.quizz[i].reponses[2].etat)">{{questions.quizz[i].reponses[2].texte}}</a>
 							<a class="waves-effect waves-light btn-large col s5 offset-s2" @click="reponse(questions.quizz[i].reponses[3].etat)">{{questions.quizz[i].reponses[3].texte}}</a>
+						</div>
+					</div>
+					<div v-if="!preloader">
+						<div class="row">
+							<button class="waves-effect waves-light btn-large col s5" v-bind:class="{'red accent-4': !questions.quizz[i].reponses[0].etat, 'lime accent-4': questions.quizz[i].reponses[0].etat}">{{questions.quizz[i].reponses[0].texte}}</button>
+							<a class="waves-effect waves-light btn-large col s5 offset-s2" v-bind:class="{'red accent-4': !questions.quizz[i].reponses[1].etat, 'lime accent-4': questions.quizz[i].reponses[1].etat}">{{questions.quizz[i].reponses[1].texte}}</a>
+						</div>
+						<div class="row">
+							<a class="waves-effect waves-light btn-large col s5" v-bind:class="{'red accent-4': !questions.quizz[i].reponses[2].etat, 'lime accent-4': questions.quizz[i].reponses[2].etat}">{{questions.quizz[i].reponses[2].texte}}</a>
+							<a class="waves-effect waves-light btn-large col s5 offset-s2" v-bind:class="{'red accent-4': !questions.quizz[i].reponses[3].etat, 'lime accent-4': questions.quizz[i].reponses[3].etat}">{{questions.quizz[i].reponses[3].texte}}</a>
 						</div>
 					</div>
 					<div class="row" v-if="preloader">
@@ -99,6 +109,9 @@ export default {
 			this.inter = setInterval(this.timer, 1000)
 			this.preloader = true
 			this.temps = 20
+			this.vert = true
+			this.orange = false
+			this.red = false
 		},
 		timer(){
 			if (this.temps == 11) {
@@ -120,8 +133,9 @@ export default {
 			this.preloader = false
 			if(state){
 				this.messageReponse = "Bonne réponse !"
-				if(this.temps)
-				this.score = this.score + 5
+				if(this.temps){
+					this.score = this.score + 5
+				}
 			}else{
 				this.messageReponse = "C'est faux"
 			}
